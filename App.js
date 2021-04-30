94a28717fac8fbd460121df344e6233aefd5ca9f
Jack Bennett <bennettjack44@gmail.com>
2021-04-30 16:12:29 +1200

    added permissions functionality

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Image } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
 
 import WelcomeScreen from './app/pages/WelcomeScreen'
 import ViewImageScreen from './app/pages/ViewImageScreen'
+import Screen from './app/pages/Screen'
 
 export default function App() {
+  const [imageUri, setImageUri] = useState()
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (!granted) {
+      alert('You need to enable permission to access the library.')
+    }
+  }
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync()
+      if (!result.cancelled) {
+        setImageUri(result.uri)
+      }
+    } catch (error) {
+      console.log('Error reading an image', error)
+    }
+  }
+  
+  useEffect(() => {
+    requestPermission()
+  }, [])
+
   return (
-    // <WelcomeScreen />
-    <ViewImageScreen />
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+    // <ViewImageScreen />
   )
 }
