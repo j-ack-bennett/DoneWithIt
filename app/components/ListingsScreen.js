2175ce3807d191867a74a159dbefedb884cd881f
j-ack-bennett <bennettjack44@gmail.com>
2021-05-04 16:47:12 +1200

    listings screen component now shows listings for sale

--- app/components/ListingsScreen.js
+++ app/components/ListingsScreen.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
 
-export default function ListingsScreen(props) {
+import colors from '../config/colors'
+import Card from '../components/Card'
+import Screen from '../components/Screen'
+
+const listings = [
+  {
+    id: 1,
+    title: 'Red jacket for sale',
+    price: 100,
+    image: require('../assets/jacket.jpg')
+  },
+  {
+    id: 2,
+    title: 'Couch in great condition',
+    price: 1000,
+    image: require('../assets/couch.jpg')
+  }
+]
+
+export default function ListingsScreen({props}) {
   return (
-    <View style={styles.container}></View>
+    <Screen style={styles.screen}>
+      <FlatList 
+        data={listings}
+        keyExtractor={listing => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card 
+            title={item.title}
+            subTitle={'$' + item.price}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light
+  }
 })
