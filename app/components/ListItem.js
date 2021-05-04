743db117fb1ec7743076663e1d5ad5201036c61d
j-ack-bennett <bennettjack44@gmail.com>
2021-05-04 16:07:40 +1200

    added props and functionality to swipe

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,30 +1,43 @@
 import React from 'react'
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Image, TouchableHighlight } from 'react-native'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 
 import colors from '../config/colors'
 import AppText from './AppText'
 
-export default function ListItem({title, subTitle, image}) {
+export default function ListItem({title, subTitle, image, ImageComponent, onPress, renderRightActions}) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image} />
-      <View>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
-      </View>
-    </View>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight 
+        underlayColor={colors.light}
+        onPress={onPress}
+      >
+        <View style={styles.container}>
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    padding: 15
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: 'center'
   },
   image: {
     width: 70,
     height: 70,
-    borderRadius: 35,
-    marginRight: 10
+    borderRadius: 35
   },
   subTitle: {
     color: colors.medium
