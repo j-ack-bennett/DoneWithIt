be25f87a6a48859dabf6def93a80c2dc7bd6355b
Jack Bennett <bennettjack44@gmail.com>
2021-05-05 13:47:02 +1200

    Testing login screen

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Image, Text, TextInput, View, Switch } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-import AppButton from './app/components/AppButton'
-import AppPicker from './app/components/AppPicker'
-import AppText from './app/components/AppText'
-import AppTextInput from './app/components/AppTextInput'
-import Card from './app/components/Card'
-import Icon from './app/components/Icon'
-import ImageInput from './app/components/ImageInput'
-import ListItem from './app/components/ListItem'
-import ListingsScreen from './app/components/ListingsScreen'
-import Screen from './app/components/Screen'
-import AccountScreen from './app/screens/AccountScreen'
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
-import MessagesScreen from './app/screens/MessagesScreen'
-import WelcomeScreen from './app/screens/WelcomeScreen'
-import ViewImageScreen from './app/screens/ViewImageScreen'
-
-const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'Clothing', value: 2 },
-  { label: 'Cameras', value: 3 }
-]
+import LoginScreen from './app/screens/LoginScreen'
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0])
   const [imageUri, setImageUri] = useState()
-
 
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
@@ -53,16 +29,7 @@ export default function App() {
   }, [])
 
   return (
-    <Screen>
-      <AppPicker 
-        items={categories}
-        selectedItem={category}
-        onSelectItem={item => setCategory(item)}
-        icon='apps' 
-        placeholder='Category' 
-      />
-      <AppTextInput icon='email' placeholder='Email' />
-    </Screen>
+    <LoginScreen />
 
     // <Screen>
     //   <Button title='Select Image' onPress={selectImage} />
