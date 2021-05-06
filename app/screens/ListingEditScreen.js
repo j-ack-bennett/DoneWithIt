521296155876d66e36af29d7670da4203b06bf00
j-ack-bennett <bennettjack44@gmail.com>
2021-05-06 23:20:42 +1200

    added more categories and props

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import * as yup from 'yup'
 
-import Screen from '../components/Screen'
 import Appform from '../components/forms/AppForm'
 import AppFormField from '../components/forms/AppFormField'
 import AppFormPicker from '../components/forms/AppFormPicker'
+import CategoryPickerItem from '../components/CategoryPickerItem'
+import Screen from '../components/Screen'
 import SubmitButton from '../components/forms/SubmitButton'
 
 const validationSchema = yup.object().shape({
@@ -31,9 +32,60 @@ const validationSchema = yup.object().shape({
 })
 
 const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'Clothing', value: 2 },
-  { label: 'Camera', value: 3 }
+  {
+    backgroundColor: "#fc5c65",
+    icon: "floor-lamp",
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    backgroundColor: "#fd9644",
+    icon: "car",
+    label: "Cars",
+    value: 2,
+  },
+  {
+    backgroundColor: "#fed330",
+    icon: "camera",
+    label: "Cameras",
+    value: 3,
+  },
+  {
+    backgroundColor: "#26de81",
+    icon: "cards",
+    label: "Games",
+    value: 4,
+  },
+  {
+    backgroundColor: "#2bcbba",
+    icon: "shoe-heel",
+    label: "Clothing",
+    value: 5,
+  },
+  {
+    backgroundColor: "#45aaf2",
+    icon: "basketball",
+    label: "Sports",
+    value: 6,
+  },
+  {
+    backgroundColor: "#4b7bec",
+    icon: "headphones",
+    label: "Movies & Music",
+    value: 7,
+  },
+  {
+    backgroundColor: "#a55eea",
+    icon: "book-open-variant",
+    label: "Books",
+    value: 8,
+  },
+  {
+    backgroundColor: "#778ca3",
+    icon: "application",
+    label: "Other",
+    value: 9,
+  },
 ]
 
 export default function ListingEditScreen() {
@@ -59,11 +111,15 @@ export default function ListingEditScreen() {
           maxLength={8}
           name='price'
           placeholder='Price'
+          width={120}
         />
         <AppFormPicker 
           items={categories}
           name='category'
+          numberOfColumns={3}
+          PickerItemComponent={CategoryPickerItem}
           placeholder='Category'
+          width='50%'
         />
         <AppFormField 
           maxLength={255}
