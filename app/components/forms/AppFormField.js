eae1fe716fa7304ec23f0a8d3a14c4930477e90e
j-ack-bennett <bennettjack44@gmail.com>
2021-05-06 23:19:35 +1200

    added width as a prop

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -4,7 +4,7 @@ import { useFormikContext } from 'formik'
 import AppTextInput from '../AppTextInput'
 import ErrorMessage from './ErrorMessage'
 
-export default function AppFormField({ name, ...otherProps }) {
+export default function AppFormField({ name, width, ...otherProps }) {
   const {errors, touched, handleChange, setFieldTouched} = useFormikContext()
 
   return (
@@ -12,6 +12,7 @@ export default function AppFormField({ name, ...otherProps }) {
       <AppTextInput 
         onBlur={() => setFieldTouched(name)}
         onChangeText={handleChange(name)}
+        width={width}
         {...otherProps}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
