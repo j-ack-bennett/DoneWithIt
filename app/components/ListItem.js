04fcf6476332ffd990e5e805761a553f4f484c3a
j-ack-bennett <bennettjack44@gmail.com>
2021-05-06 23:18:52 +1200

    added material icon and number of lines limit

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, StyleSheet, Image, TouchableHighlight } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import Swipeable from 'react-native-gesture-handler/Swipeable'
 
 import colors from '../config/colors'
@@ -16,9 +17,18 @@ export default function ListItem({title, subTitle, image, IconComponent, onPress
           {IconComponent}
           {image && <Image style={styles.image} source={image} />}
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+            <AppText style={styles.title} numberOfLines={1}>{title}</AppText>
+            {subTitle && (
+              <AppText style={styles.subTitle} numberOfLines={2}>
+                {subTitle}
+              </AppText>
+            )}
           </View>
+          <MaterialCommunityIcons 
+            color={colors.medium} 
+            name='chevron-right' 
+            size={25} 
+          />
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -29,9 +39,11 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     padding: 15,
-    backgroundColor: colors.white 
+    backgroundColor: colors.white,
+    alignItems: 'center'
   },
   detailsContainer: {
+    flex: 1,
     marginLeft: 10,
     justifyContent: 'center'
   },
