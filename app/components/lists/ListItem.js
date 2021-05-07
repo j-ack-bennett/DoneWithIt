de95548c69e353845145b155ce248bfbc6d70220
Jack Bennett <bennettjack44@gmail.com>
2021-05-07 12:05:25 +1200

    moved to new lists folder

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -3,8 +3,8 @@ import { View, StyleSheet, Image, TouchableHighlight } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import Swipeable from 'react-native-gesture-handler/Swipeable'
 
-import colors from '../config/colors'
-import AppText from './AppText'
+import colors from '../../config/colors'
+import AppText from '../AppText'
 
 export default function ListItem({title, subTitle, image, IconComponent, onPress, renderRightActions}) {
   return (
