eab0273dde86639cf78b79cd57ff4ce0e9f05248
Jack Bennett <bennettjack44@gmail.com>
2021-05-07 14:42:01 +1200

    changing around component screens for testing

--- App.js
+++ App.js
@@ -1,39 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import * as ImagePicker from 'expo-image-picker'
+import React from 'react'
 
-import LoginScreen from './app/screens/LoginScreen'
+import ListingEditScreen from './app/screens/ListingEditScreen'
 
 export default function App() {
-  const [imageUri, setImageUri] = useState()
-
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
-    if (!granted) {
-      alert('You need to enable permission to access the library.')
-    }
-  }
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync()
-      if (!result.cancelled) {
-        setImageUri(result.uri)
-      }
-    } catch (error) {
-      console.log('Error reading an image', error)
-    }
-  }
-  
-  useEffect(() => {
-    requestPermission()
-  }, [])
-
   return (
-    <LoginScreen />
-    // <Screen>
-    //   <Button title='Select Image' onPress={selectImage} />
-    //   <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-    //   <ImageInput imageUri={imageUri} />
-    // </Screen>
+    <ListingEditScreen />
   )
 }
