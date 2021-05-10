2af80075adc933d87d49128b767238b9ce8330b6
j-ack-bennett <bennettjack44@gmail.com>
2021-05-11 09:28:47 +1200

    added route prop to view all listing information

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -5,13 +5,15 @@ import colors from '../config/colors'
 import AppText from '../components/AppText'
 import ListItem from '../components/lists/ListItem'
 
-export default function ListingDetailsScreen(props) {
+export default function ListingDetailsScreen({ route }) {
+  const listing = route.params
+
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+      <Image style={styles.image} source={listing.image} />
       <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>Red jacket for sale</AppText>
-        <AppText style={styles.price}>$100</AppText>
+        <AppText style={styles.title}>{listing.title}</AppText>
+        <AppText style={styles.price}>{listing.price}</AppText>
         <View style={styles.userContainer}>
           <ListItem 
             image={require('../assets/mosh.jpg')}
