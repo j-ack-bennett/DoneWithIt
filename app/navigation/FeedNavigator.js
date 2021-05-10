e1da532cf4da99c138fbb3a59e312ae304de9b70
j-ack-bennett <bennettjack44@gmail.com>
2021-05-11 09:26:33 +1200

    created navigation for feed screen including modal transition

---
+++ app/navigation/FeedNavigator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import ListingDetailsScreen from '../screens/ListingDetailsScreen'
+import ListingsScreen from '../screens/ListingsScreen'
+
+const Stack = createStackNavigator()
+
+export default function FeedNavigator() {
+  return(
+    <Stack.Navigator mode='modal'>
+      <Stack.Screen name='Listings' component={ListingsScreen} />
+      <Stack.Screen name='ListingDetails' component={ListingDetailsScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  )
+}
