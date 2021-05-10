a1a43b55010c28d9410114062fa13c5eb0354dd7
j-ack-bennett <bennettjack44@gmail.com>
2021-05-11 09:23:17 +1200

    testing navigation

--- App.js
+++ App.js
@@ -1,72 +1,82 @@
 import React from 'react'
-import { Button, Text } from 'react-native'
-import { createStackNavigator} from '@react-navigation/stack'
-import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
 
-import Screen from './app/components/Screen'
-import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio'
+import AuthNavigator from './app/navigation/AuthNavigator'
+import NavigationTheme from './app/navigation/NavigationTheme'
+import AppNavigator from './app/navigation/AppNavigator'
 
-const Link = () => {
-  const navigation = useNavigation()
+// const Link = () => {
+//   const navigation = useNavigation()
 
-  return (
-    <Button title='Click' onPress={() => navigation.navigate('TweetDetails', { id: 1 })} />
-  )
-}
+//   return (
+//     <Button title='Click' onPress={() => navigation.navigate('TweetDetails', { id: 1 })} />
+//   )
+// }
 
-const Tweets = () => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-)
+// const Tweets = () => (
+//   <Screen>
+//     <Text>Tweets</Text>
+//     <Link />
+//   </Screen>
+// )
 
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
-  </Screen>
-)
+// const TweetDetails = ({ route }) => (
+//   <Screen>
+//     <Text>Tweet Details {route.params.id}</Text>
+//   </Screen>
+// )
 
-const Stack = createStackNavigator()
-const StackNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: {backgroundColor: 'dodgerblue'},
-      headerTintColor: 'white',
-    }}
-  >
-    <Stack.Screen 
-      name='Tweets' 
-      component={Tweets} 
-      options={{
-        headerStyle: {backgroundColor: 'tomato'},
-        headerTintColor: 'white',
-        headerShown: false
-      }}
-    />
-    <Stack.Screen 
-      name='TweetDetails' 
-      component={TweetDetails} 
-      options={({ route }) => ({ title: route.params.id })}
-    />
-  </Stack.Navigator>
-)
+// const Stack = createStackNavigator()
 
-const Account = () => <Screen><Text>Account</Text></Screen>
+// const FeedNavigator = () => (
+//   <Stack.Navigator
+//     screenOptions={{
+//       headerStyle: {backgroundColor: 'dodgerblue'},
+//       headerTintColor: 'white',
+//     }}
+//   >
+//     <Stack.Screen 
+//       name='Tweets' 
+//       component={Tweets} 
+//       options={{
+//         headerStyle: {backgroundColor: 'tomato'},
+//         headerTintColor: 'white',
+//         headerShown: false
+//       }}
+//     />
+//     <Stack.Screen 
+//       name='TweetDetails' 
+//       component={TweetDetails} 
+//       options={({ route }) => ({ title: route.params.id })}
+//     />
+//   </Stack.Navigator>
+// )
 
-const Tab = createBottomTabNavigator()
-const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name='Feed' component={Tweets} />
-    <Tab.Screen name='Account' component={Account} />
-  </Tab.Navigator>
-)
+// const Account = () => <Screen><Text>Account</Text></Screen>
+
+// const Tab = createBottomTabNavigator()
+
+// const TabNavigator = () => (
+//   <Tab.Navigator
+//     tabBarOptions={{
+//       activeBackgroundColor: 'tomato',
+//       activeTintColor: 'white',
+//       inactiveBackgroundColor: '#eee',
+//       inactiveTintColor: 'black'
+//     }}
+//   >
+//     <Tab.Screen 
+//       name='Feed' 
+//       component={FeedNavigator} 
+//     />
+//     <Tab.Screen name='Account' component={Account} />
+//   </Tab.Navigator>
+// )
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigator />
+    <NavigationContainer theme={NavigationTheme}>
+      <AppNavigator />
     </NavigationContainer>
   )
 }
