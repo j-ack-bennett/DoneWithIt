ed227a9cef753db96a44f81ac0952da4201d07a5
j-ack-bennett <bennettjack44@gmail.com>
2021-05-11 10:03:59 +1200

    imported and updated routes

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, View, ImageBackground, Image, Text } from 'react-native'
 
 import AppButton from '../components/AppButton'
+import routes from '../navigation/routes'
 
 export default function WelcomeScreen({ navigation }) {
   return (
@@ -15,8 +16,8 @@ export default function WelcomeScreen({ navigation }) {
         <Text style={styles.tagLine}>Sell What You Don't Need</Text>
       </View>
       <View style={styles.buttonsContainer}>
-        <AppButton title='Login' onPress={() => navigation.navigate('Login')} />
-        <AppButton title='Register' color='secondary' onPress={() => navigation.navigate('Register')} />
+        <AppButton title='Login' onPress={() => navigation.navigate(routes.LOGIN)} />
+        <AppButton title='Register' color='secondary' onPress={() => navigation.navigate(routes.REGISTER)} />
       </View>
     </ImageBackground>
   )
