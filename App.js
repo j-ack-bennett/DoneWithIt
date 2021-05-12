06fd31bff995d43e3de4018d388bb03f89fbfadd
Jack Bennett <bennettjack44@gmail.com>
2021-05-12 17:50:40 +1200

    removed contents

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import { LogBox } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-
-import NavigationTheme from './app/navigation/NavigationTheme'
-import AppNavigator from './app/navigation/AppNavigator'
 
 LogBox.ignoreLogs([
   "It appears that you are using old version of react-navigation library",
@@ -11,8 +7,6 @@ LogBox.ignoreLogs([
 
 export default function App() {
   return (
-    <NavigationContainer theme={NavigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    null
   )
 }
