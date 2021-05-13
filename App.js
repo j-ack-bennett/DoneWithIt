a82915dcdb5111324020dad81c906569626e76fd
j-ack-bennett <bennettjack44@gmail.com>
2021-05-13 12:22:34 +1200

    testing app navigator for offline cache

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { NavigationContainer } from "@react-navigation/native";
 import { LogBox } from 'react-native'
+
+import AppNavigator from './app/navigation/AppNavigator'
+import NavigationTheme from './app/navigation/NavigationTheme'
+import OfflineNotice from './app/components/OfflineNotice';
 
 LogBox.ignoreLogs([
   "It appears that you are using old version of react-navigation library",
@@ -7,6 +12,11 @@ LogBox.ignoreLogs([
 
 export default function App() {
   return (
-    null
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={NavigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   )
 }
