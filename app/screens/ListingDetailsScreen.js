8ad5880be4021ce9f2bd0d757530243d077eddd5
j-ack-bennett <bennettjack44@gmail.com>
2021-05-13 12:25:04 +1200

    changed to image cache component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Image } from 'react-native-expo-image-cache'
 
 import colors from '../config/colors'
 import AppText from '../components/AppText'
@@ -10,7 +11,7 @@ export default function ListingDetailsScreen({ route }) {
 
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={listing.image} />
+      <Image style={styles.image} preview={{ uri: listing.images[0].thumbnailUrl }} tint='light' uri={listing.images[0].url} />
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>{listing.title}</AppText>
         <AppText style={styles.price}>{listing.price}</AppText>
