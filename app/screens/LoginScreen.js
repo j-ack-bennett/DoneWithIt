3e20671bec60096da58fe6670e58cd2e67dda988
j-ack-bennett <bennettjack44@gmail.com>
2021-05-13 16:46:31 +1200

    added handlesubmit for user information on loginscreen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Image, StyleSheet } from 'react-native'
 import * as yup from 'yup'
+import jwtDecode from 'jwt-decode'
 
 import Screen from '../components/Screen'
 import AppForm from '../components/forms/AppForm'
 import AppFormField from '../components/forms/AppFormField'
+import authApi from '../api/auth'
+import AuthContext from '../auth/context'
+import ErrorMessage from '../components/forms/ErrorMessage'
 import SubmitButton from '../components/forms/SubmitButton'
 
 const validationSchema = yup.object().shape({
@@ -18,21 +22,33 @@ const validationSchema = yup.object().shape({
     .required()
     .min(4, 'Password is too short')
     .label('Password')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-    )
+    // .matches(
+    //   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+    //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+    // )
 })
 
 export default function LoginScreen() {
+  const authContext = useContext(AuthContext)
+  const [loginFailed, setLoginFailed] = useState(false)
+
+  const handleSubmit = async ({ email, password }) => {
+    const result = await authApi.login(email, password)
+    if (!result.ok) return setLoginFailed(true)
+    setLoginFailed(false)
+    const user = jwtDecode(result.data)
+    authContext.setUser(user)
+  }
+
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require('../assets/logo-red.png')} />
       <AppForm
         initialValues={{ email: '', password: '' }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
+        <ErrorMessage error='Invalid email and/or password.' visible={loginFailed} />
         <AppFormField
           autoCapitalize='none'
           autoCorrect={false}
