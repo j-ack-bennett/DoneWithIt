55ff845b20578f30fa668d18d4ac1f02bdab7a26
j-ack-bennett <bennettjack44@gmail.com>
2021-05-13 16:44:34 +1200

    added auth provider

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { LogBox } from 'react-native'
 
 import AppNavigator from './app/navigation/AppNavigator'
+import AuthNavigator from './app/navigation/AuthNavigator';
 import NavigationTheme from './app/navigation/NavigationTheme'
 import OfflineNotice from './app/components/OfflineNotice';
+import AuthContext from './app/auth/context';
 
 LogBox.ignoreLogs([
   "It appears that you are using old version of react-navigation library",
 ])
 
 export default function App() {
+  const [user, setUser] = useState()
+
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={NavigationTheme}>
-        <AppNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   )
 }
