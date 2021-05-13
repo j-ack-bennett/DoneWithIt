b31189df06700a6484f17efb6c7703981e08a655
j-ack-bennett <bennettjack44@gmail.com>
2021-05-13 23:30:26 +1200

    added conditional rendering for authenticated users

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
-import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from 'expo-app-loading'
+import { NavigationContainer } from "@react-navigation/native"
 import { LogBox } from 'react-native'
 
 import AppNavigator from './app/navigation/AppNavigator'
-import AuthNavigator from './app/navigation/AuthNavigator';
+import AuthNavigator from './app/navigation/AuthNavigator'
 import NavigationTheme from './app/navigation/NavigationTheme'
-import OfflineNotice from './app/components/OfflineNotice';
-import AuthContext from './app/auth/context';
+import OfflineNotice from './app/components/OfflineNotice'
+import AuthContext from './app/auth/context'
+import authStorage from './app/auth/storage'
 
 LogBox.ignoreLogs([
   "It appears that you are using old version of react-navigation library",
@@ -14,6 +16,21 @@ LogBox.ignoreLogs([
 
 export default function App() {
   const [user, setUser] = useState()
+  const [isReady, setIsReady] = useState(false)
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser()
+    if (user) setUser(user)
+  }
+
+  if (!isReady)
+    return (
+      <AppLoading 
+        startAsync={restoreUser} 
+        onFinish={() => setIsReady(true)} 
+        onError={console.warn} 
+      />
+    )
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
